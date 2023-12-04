e9e77600d1f42c739348d12aa75938c14bea5c15
Eddy PEPY <eddy.pepy.ext@orange.com>
2023-12-04 14:21:47 +0100

    Update terraform code

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-west-1"
+  region = "eu-central-1"
 }
 
 resource "aws_apprunner_connection" "github" {
@@ -21,8 +21,21 @@ resource "aws_apprunner_connection" "github" {
   }
 }
 
+resource "aws_apprunner_observability_configuration" "xray" {
+  observability_configuration_name = "xray"
+
+  trace_configuration {
+    vendor = "AWSXRAY"
+  }
+}
+
 resource "aws_apprunner_service" "websocketserver" {
   service_name = "websocket-server"
+
+  observability_configuration {
+    observability_configuration_arn = aws_apprunner_observability_configuration.xray.arn
+    observability_enabled           = true
+  }
 
   source_configuration {
     authentication_configuration {
@@ -31,20 +44,27 @@ resource "aws_apprunner_service" "websocketserver" {
     code_repository {
       code_configuration {
         code_configuration_values {
-          build_command = "python -m pip install websockets"
+          build_command = "pip install -r requirements.txt"
           port          = "8765"
           runtime       = "PYTHON_3"
           start_command = "python app.py"
         }
         configuration_source = "API"
       }
-      repository_url = "git@github.com:arongate/thin-websocket-app.git"
+      repository_url = "https://github.com/arongate/tiny-websocket-app"
       source_code_version {
         type  = "BRANCH"
         value = "main"
       }
       source_directory = "server"
     }
+  }
+
+  health_check_configuration {
+    interval            = 3
+    healthy_threshold   = 2
+    protocol            = "TCP"
+    unhealthy_threshold = 10
   }
 
   network_configuration {
